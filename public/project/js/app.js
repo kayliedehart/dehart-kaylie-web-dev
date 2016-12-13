71d5376651349dd35e0bb165fd4367e6613182eb
kdehart <kdehart@ccs.neu.edu>
2016-12-12 19:44:38 -0800

    angular routing, controllers fleshed out (a little), update forms inputs to use user object

--- public/project/js/app.js
+++ public/project/js/app.js
@@ -1,57 +1,90 @@
 (function() {
-    angular
-        .module("MASHApp", [])
-        .controller("LoginController", LoginController)
-        .filter('reverse', function() {
-            return function(items) {
-                return items.slice().reverse();
+    var app = angular.module("MASHApp", ["ngRoute", 'services']);
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/app,', {
+                templateUrl: "../html/login.html",
+                controller:'LoginController'
+            })
+            .when('/login', {
+                templateUrl: "../html/login.html",
+                controller:'LoginController'
+            })
+            .when('/register', {
+                templateUrl: "../html/register.html",
+                controller:'RegisterController'
+            })
+            .when('/details', {
+                templateUrl: "../html/details.html",
+                controller:'ProfileController'
+            })
+            .when('/mash', {
+                templateUrl: "../html/mash.html",
+                controller:'MASHController'
+            })
+            .otherwise({
+                template: 'does not exists'
+            });
+    });
+
+    app.controller('LoginController',
+        function LoginController($scope, services) {
+            $scope.validate = function (user) {
+
+            }
+        }
+    );
+
+    app.controller('RegisterController',
+        function RegisterController($scope, services) {
+
+            $scope.userInfo = function (user) {
+
+            }
+        }
+    );
+
+    app.controller('ProfileController',
+        function ProfileController($scope, services) {
+
+            $scope.profileLoad = function (){
+
             };
-        });
 
-    function LoginController($http) {
-        var vm = this;
-        vm.createMessage = createMessage;
-        vm.deleteMessage = deleteMessage;
+            $scope.deleteConfirm = function () {
 
-        function init() {
-            findAllMessages();
-        }
-        init();
-
-        function createMessage(message) {
-            vm.message = "";
-            var obj = {
-                message: message
             };
-            $http.post("/api/test", obj)
-                .then(
-                    findAllMessages,
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
-        }
 
-        function deleteMessage(message) {
-            $http.delete("/api/test/" + message._id)
-                .then(
-                    findAllMessages,
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
-        }
+            $scope.processMASH = function(user){
 
-        function findAllMessages() {
-            $http.get("/api/test")
-                .then(
-                    function(response) {
-                        vm.messages = response.data;
-                    },
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
+            };
+
         }
-    }
+    );
+
+    app.controller('MASHController',
+        function MASHController($scope, services) {
+            $scope.getALife = function (){
+
+            };
+
+            $scope.save = function (user) {
+
+            };
+        }
+    );
+
+
+    var services_mod = angular.module("services", []);
+
+    services_mod.factory('services', function() {
+        this.location = ['Boston, MA', 'San Fransisco, CA', 'New York City, NY', 'Washington, DC', 'Denver, CO'];
+        this.age;
+        this.major = ['Computer Science', 'Political Science', 'Physics', 'Engineering', 'English', 'Art', 'History'];
+        this.gender = ['Female', 'Male', 'Non-Binary', 'Other'];
+        this.orientation = ['Straight', 'Gay', 'Bisexual', 'Pansexual', 'Asexual'];
+        return {};
+    });
+
 })();
