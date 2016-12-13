40f271ae1693de70156ece0d94cd41727fe08bf8
kdehart <kdehart@ccs.neu.edu>
2016-12-13 12:44:43 -0800

    redundant MASH function, typo, fixed drop downs in register

--- public/project/js/app.js
+++ public/project/js/app.js
@@ -24,53 +24,84 @@
                 controller:'MASHController'
             })
             .otherwise({
-                template: 'does not exists'
+                template: 'does not exist'
             });
     });
 
     app.controller('LoginController',
-        function LoginController($scope, services) {
+        function LoginController($scope, $http, services) {
             $scope.validate = function (user) {
-
-            }
+                $http.get('/rest/userProfile/:name:password', user.username, user.password)
+                    .then(function(response) {
+                        $scope.id = response._id;
+                    }, function (response) {
+                    $scope.window.alert("Invalid login");
+                });
+            };
         }
     );
 
     app.controller('RegisterController',
-        function RegisterController($scope, services) {
+        function RegisterController($scope, $http, services) {
+
+            $scope.location = ['Boston, MA', 'San Fransisco, CA', 'New York City, NY', 'Washington, DC', 'Denver, CO'];
+            $scope.major = ['Computer Science', 'Political Science', 'Physics', 'Engineering', 'English', 'Art', 'History'];
+            $scope.gender = ['Female', 'Male', 'Non-Binary', 'Other'];
+            $scope.orientation = ['Straight', 'Gay', 'Bisexual', 'Pansexual', 'Asexual'];
 
             $scope.userInfo = function (user) {
+                $http.post(BASE_URL + '/rest/userProfile', user)
+                    .then(function success(response){
+                        $scope._id = response._id;
+                    }, function failure(response) {
 
-            }
+                });
+            };
         }
     );
 
     app.controller('ProfileController',
-        function ProfileController($scope, services) {
+        function ProfileController($scope, $http, services) {
 
             $scope.profileLoad = function (){
+                $http.get(BASE_URL + 'rest/userProfile/:id')
+                    .then(function success(response){
+                        $scope.user = response;
+                    }, function failure(response) {
 
+                    });
             };
 
             $scope.deleteConfirm = function () {
+                $scope.window.alert("Are you sure you want to delete your profile?");
+                $http.delete(BASE_URL + 'rest/userProfile/:id')
+                    .then(function success(response){
+                        $scope.user = false;
+                    }, function failure(response) {
 
+                    });
             };
-
-            $scope.processMASH = function(user){
-
-            };
-
         }
     );
 
     app.controller('MASHController',
-        function MASHController($scope, services) {
+        function MASHController($scope, $http, services) {
             $scope.getALife = function (){
+                $http.get()
+                    .then(function success(response){
 
+                    }, function failure(response) {
+
+                    });
             };
 
             $scope.save = function (user) {
+                $http.put(BASE_URL + 'rest/userProfile/:id')
+                    .then(function success(response){
+                        $scope.user = response.user;
+                    }, function failure(response) {
 
+                    });
             };
         }
     );
@@ -79,11 +110,6 @@
     var services_mod = angular.module("services", []);
 
     services_mod.factory('services', function() {
-        this.location = ['Boston, MA', 'San Fransisco, CA', 'New York City, NY', 'Washington, DC', 'Denver, CO'];
-        this.age;
-        this.major = ['Computer Science', 'Political Science', 'Physics', 'Engineering', 'English', 'Art', 'History'];
-        this.gender = ['Female', 'Male', 'Non-Binary', 'Other'];
-        this.orientation = ['Straight', 'Gay', 'Bisexual', 'Pansexual', 'Asexual'];
         return {};
     });
 
